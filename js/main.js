1778880abb825d72c0a572f88ccb603ad4b78d19
khainguyen374 <55184694+khainguyen374@users.noreply.github.com>
2022-02-09 10:33:56 +0700

    push
    push

---
+++ js/main.js
@@ -0,0 +1,165 @@
+new WOW().init();
+jQuery(document).ready(function() {
+  jQuery('#imageGallery').lightSlider({
+  gallery:true,
+  item:1,
+  loop:true,
+  thumbItem:4,
+  slideMargin:0,
+  enableDrag: false,
+  currentPagerPosition:'left',
+  onSliderLoad: function(el) {
+  el.lightGallery({
+  selector: '#imageGallery .lslide'
+  });
+  }
+  });
+});
+$('.responsive').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 6,
+    autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+$('.slider-product').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+// back to top
+jQuery(document).ready(function($){
+
+  $('.popup-overlay').click(function(){
+      $(this).removeClass('click');
+      $('.my-popup').removeClass('click');
+   });
+
+  $('.lh-show-bk').click(function(){
+    $('.popup-overlay').addClass('click');
+    $('.my-popup').addClass('click');
+  });
+  $('.exit').click(function(){
+     $('.popup-overlay').removeClass('click');
+    $(this).parents('.my-popup').removeClass('click');
+  });
+// end popup
+
+
+  if($(".btn-top").length > 0){
+    $(window).scroll(function () {
+      var e = $(window).scrollTop();
+      if (e > 400) {
+        $(".btn-top").show();
+      } else {
+        $(".btn-top").hide()
+      }
+    });
+    $(".btn-top").click(function () {
+      $('body,html').animate({
+        scrollTop: 0
+      })
+    })
+  }
+});
+// end backto top
+// /menu mobi/
+$(document).ready(function () {
+       $("#menu").click(function(){
+    	   $("#drop_down").mmenu({
+            offCanvas: {
+                position: "right",
+            }
+        });
+    	$("#drop_down").mmenu();
+    	   var API=$("#drop_down").data("mmenu");
+            API.open();
+        });
+
+  });
+    //
+ //
+ $(document).on('click', '.next', function(){
+ $(this).siblings('.slick-slider').slick('slickNext');
+});
+
+$(document).on('click', '.prev', function(){
+  $(this).siblings('.slick-slider').slick('slickPrev');
+});
+
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+jQuery(document).scroll(function(){
+  if(jQuery(this).scrollTop() > 500)
+  {   
+     jQuery('.header-position').css({"background":"#585858","transition-delay":"0.3s"});
+     jQuery('.block-menu').css({"margin-top":"0px","transition-delay":"0.3s"});
+     jQuery('.block-menu ul li').css({"border-bottom":"0px","margin":"0px","transition-delay":"0.3s"});
+  } else {
+     jQuery('.header-position').css({"background":"transparent"});
+     jQuery('.block-menu').css({"margin-top":"20px"});
+     jQuery('.block-menu ul li').css({"border-bottom":"0px solid #FFF","margin":"0"});
+     
+  }
+});
